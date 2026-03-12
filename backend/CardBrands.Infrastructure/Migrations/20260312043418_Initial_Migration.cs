c082a4e735b2942b9e016bd20c7c6a7553d96840
Eddy Barahona <barahona.hernandez2712@gmail.com>
2026-03-11 22:38:24 -0600

    Agregar Initial_Migration

---
+++ backend/CardBrands.Infrastructure/Migrations/20260312043418_Initial_Migration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardBrands.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MarcasAutos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    Name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    OriginCountry = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    FoundingDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    WebSite = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Value = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    EditedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    EditedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    DeletedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarcasAutos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MarcasAutos");
+        }
+    }
+}
