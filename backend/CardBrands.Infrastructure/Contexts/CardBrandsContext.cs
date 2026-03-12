860e62dfeb189f6a2c69d9055ad19f8040beebde
Eddy Barahona <barahona.hernandez2712@gmail.com>
2026-03-11 22:05:25 -0600

    backend: Agregar CardBrandsContext

---
+++ backend/CardBrands.Infrastructure/Contexts/CardBrandsContext.cs
@@ -0,0 +1,67 @@
+﻿using CardBrands.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CardBrands.Infrastructure.Contexts;
+
+public class CardBrandsContext(
+    DbContextOptions<CardBrandsContext> options) : DbContext(options)
+{
+    public DbSet<MarcaAuto> MarcaAutos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Apply configurations
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CardBrandsContext).Assembly);
+
+        // Aplicar Soft Delete global filter
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                modelBuilder.Entity(entityType.ClrType)
+                    .HasQueryFilter(GetIsDeletedRestriction(entityType.ClrType));
+            }
+        }
+    }
+
+    private static LambdaExpression GetIsDeletedRestriction(Type type)
+    {
+        var parameter = Expression.Parameter(type, "e");
+        var prop = Expression.Property(parameter, nameof(BaseEntity.IsDeleted));
+        var condition = Expression.Equal(prop, Expression.Constant(false));
+        return Expression.Lambda(condition, parameter);
+    }
+
+    public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        var now = DateTimeOffset.UtcNow;
+
+        var entries = ChangeTracker
+            .Entries<BaseEntity>();
+
+        foreach (var entry in entries)
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.SetCreated("SYSTEM", now);
+                    break;
+
+                case EntityState.Modified:
+                    entry.Entity.SetEdited("SYSTEM", now);
+                    break;
+
+                case EntityState.Deleted:
+                    entry.State = EntityState.Modified;
+                    entry.Entity.MarkAsDeleted("SYSTEM", now);
+                    break;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
